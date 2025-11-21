299db247608669ccc83a5ea753019b112445204f
ratsnest1 <michalevstratov1@gmail.com>
2025-11-21 23:05:06 +0300

    Delete evstratovmain directory

--- evstratovmain/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const title = document.querySelector('.fixed-title');
-    setTimeout(() => {
-        title.style.opacity = '1';
-    }, 500);
-});
