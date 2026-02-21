d1b7732cae2354d562534e35f04c4acd2f6c3be6
ratsnest1 <michalevstratov1@gmail.com>
2026-02-21 16:53:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const title = document.querySelector('.fixed-title');
-    setTimeout(function() {
-        title.style.opacity = '1';
-    }, 500);
+document.addEventListener('DOMContentLoaded', function () {
+  // Показать имя в правом‑верхнем углу
+  const title = document.querySelector('.fixed-title');
+  setTimeout(() => {
+    title.style.opacity = '1';
+  }, 500);
+
+  // Показать ссылку “collections” чуть позже
+  const coll = document.querySelector('.collections-link');
+  setTimeout(() => {
+    coll.classList.add('show');
+  }, 800);
 });
