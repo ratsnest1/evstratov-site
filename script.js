d600394c34c0f8e81295c22f406cfdd414c649dc
ratsnest1 <michalevstratov1@gmail.com>
2026-02-22 13:57:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ document.addEventListener('DOMContentLoaded', function() {
   setTimeout(function() {
     title.style.opacity = '1';
   }, 500);
+
+  // Блокировка скролла на телефоне
+  document.body.addEventListener('touchmove', function(e) {
+    e.preventDefault();
+  }, { passive: false });
 });
